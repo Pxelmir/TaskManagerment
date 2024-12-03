1aa3add622e70ea9c10c305c4564bac3cbd5d8c6
Pxelmir <thisisamirbudhathoki@gmail.com>
2024-12-03 08:35:29 +0545

    Add files via upload

---
+++ dbconectedcode/Splash.java
@@ -0,0 +1,35 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Splash extends JFrame{
+    Splash(){
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("img/Welcome.jpeg"));
+        Image i2 = i1.getImage().getScaledInstance(1170 , 650, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,1170,650);
+        add(image);
+
+        setSize(1170,650);
+        setLocation(200,100);
+        setLayout(null);
+        setVisible(true);
+
+        try{
+            Thread.sleep(2000);
+            setVisible(false);
+            new MyLogin();;
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        new Splash();
+        
+    }
+
+
+}
